5f20b1bee460bf1b511afb711c7c724830235013
Si manglam <qwer09214@gmail.com>
2026-09-10 14:57:09 +0800

    Refactor main.py to enhance instruction binary formatting and improve readability

--- main.py
+++ main.py
@@ -6,9 +6,18 @@ from asmbler.passes.codeGen import CodeGen
 p = Parser("test.txt")
 code: CodeGen = CodeGen()
 
+t = Tokenizer("test.txt")
+
 for i in p.parse():
     b: bytes = i.visit(code)
-    print(bin(int.from_bytes(b, byteorder='big'))[2:7])
-    print(bin(int.from_bytes(b, byteorder='big'))[8:12])
-    print(bin(int.from_bytes(b, byteorder='big'))[13:17])
-    print(bin(int.from_bytes(b, byteorder='big'))[18:33])
+    ins: str = bin(int.from_bytes(b, byteorder='big'))
+    ins = ins.removeprefix('0b')
+    
+    while len(ins) != 32:
+        ins = "0" + ins
+    
+    print(ins)
+    #print(ins[0:5])
+    #print(ins[6:10])
+    #print(ins[10:15])
+    #print(ins[16:31])
