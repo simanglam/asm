2c42d7831b1b4c477ee1377873ad78dafc5605a3
Si manglam <qwer09214@gmail.com>
2026-09-10 14:18:05 +0800

    Refactor main.py to include CodeGen usage and update test.txt by removing unnecessary lines

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from asmbler import Tokenizer, TokenType, Parser
 
 from asmbler.util.rigister import RegisterHelper
+from asmbler.passes.codeGen import CodeGen
 
 p = Parser("test.txt")
-
+code: CodeGen = CodeGen()
 
 for i in p.parse():
-    print(i)
+    b: bytes = i.visit(code)
+    print(bin(int.from_bytes(b, byteorder='big'))[2:7])
+    print(bin(int.from_bytes(b, byteorder='big'))[8:12])
+    print(bin(int.from_bytes(b, byteorder='big'))[13:17])
+    print(bin(int.from_bytes(b, byteorder='big'))[18:33])
